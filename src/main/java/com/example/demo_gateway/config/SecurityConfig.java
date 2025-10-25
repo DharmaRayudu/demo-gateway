28b8da75773c629e4673e6d62782d5734c889704
Dharma Rayudu <dharmarayudu1991@gmail.com>
2025-10-25 20:55:12 +0530

    bean config

--- src/main/java/com/example/demo_gateway/config/SecurityConfig.java
+++ src/main/java/com/example/demo_gateway/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.example.demo_gateway.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,6 +9,7 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class SecurityConfig {
 	
+	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http
 		  .authorizeHttpRequests(request->request
